7d7716d50f21fbd988ffbfd80dbc0c1a25a69e2e
riccamarzi <riccardo.marzilli@gmail.com>
2022-10-03 07:48:18 -0700

    added openims icscf scscf

--- openimscore/ser_ims/openims_components.sh
+++ openimscore/ser_ims/openims_components.sh
@@ -1,14 +1,17 @@
 case $OPENIMS_COMPONENT in
 	pcscf)
-		cp /opt/OpenIMSCore/ser_ims/cfg/pcscf.* /opt/OpenIMSCore
+		cp /opt/OpenIMSCore/ser_ims/cfg/pcscf.sh /opt/OpenIMSCore
+		cp /mnt/pcscf.* /opt/OpenIMSCore
 		/opt/OpenIMSCore/pcscf.sh
 		;;
 	scscf)
-		cp /opt/OpenIMSCore/ser_ims/cfg/scscf.* /opt/OpenIMSCore
+		cp /opt/OpenIMSCore/ser_ims/cfg/scscf.sh /opt/OpenIMSCore
+		cp /mnt/scscf.* /opt/OpenIMSCore
 		/opt/OpenIMSCore/scscf.sh
 		;;
 	icscf)
-		cp /opt/OpenIMSCore/ser_ims/cfg/icscf.* /opt/OpenIMSCore
+		cp /opt/OpenIMSCore/ser_ims/cfg/icscf.sh /opt/OpenIMSCore
+		cp /mnt/icscf.* /opt/OpenIMSCore
 		mysql -u root -p$OPENIMS_MYSQL_PWD -h $OPENIMS_MYSQL_IP < /opt/OpenIMSCore/ser_ims/cfg/icscf.sql
 		/opt/OpenIMSCore/icscf.sh
 		;;
