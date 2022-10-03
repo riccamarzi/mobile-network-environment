7d5f9e9a46a70a1ef5752a6e7e2091e0ec967b44
riccamarzi <riccardo.marzilli@gmail.com>
2022-10-03 06:34:11 -0700

    created openims/pcscf config xml file

--- openimscore/ser_ims/openims_components.sh
+++ openimscore/ser_ims/openims_components.sh
@@ -3,4 +3,13 @@ case $OPENIMS_COMPONENT in
 		cp /opt/OpenIMSCore/ser_ims/cfg/pcscf.* /opt/OpenIMSCore
 		/opt/OpenIMSCore/pcscf.sh
 		;;
+	scscf)
+		cp /opt/OpenIMSCore/ser_ims/cfg/scscf.* /opt/OpenIMSCore
+		/opt/OpenIMSCore/scscf.sh
+		;;
+	icscf)
+		cp /opt/OpenIMSCore/ser_ims/cfg/icscf.* /opt/OpenIMSCore
+		mysql -u root -p$OPENIMS_MYSQL_PWD -h $OPENIMS_MYSQL_IP < /opt/OpenIMSCore/ser_ims/cfg/icscf.sql
+		/opt/OpenIMSCore/icscf.sh
+		;;
 esac
