8fd4236abc96536bf4355b5610ced37553a41649
riccamarzi <riccardo.marzilli@gmail.com>
2022-12-13 01:23:56 -0800

    Modified Kamailio domain, FHoSS userdata.sql

--- kamailio_docker/dns/dns_conf/dns_startup.sh
+++ kamailio_docker/dns/dns_conf/dns_startup.sh
@@ -4,6 +4,8 @@ cp /mnt/ims.openims.mnc001.mcc001.3gppnetwork.org /etc/bind
 cp /mnt/epc.mnc001.mcc001.3gppnetwork.org /etc/bind
 cp /mnt/named.conf.* /etc/bind
 
+sed -i "s/IMS_DOMAIN/$KAMAILIO_IMS_DOMAIN/g" /etc/bind/ims.mnc001.mcc001.3gppnetwork.org
+sed -i "s/IMS_DOMAIN/$KAMAILIO_IMS_DOMAIN/g" /etc/bind/named.conf.local
 sed -i "s/KAMAILIO_PCSCF_IP/$KAMAILIO_PCSCF_IP/g" /etc/bind/ims.mnc001.mcc001.3gppnetwork.org
 sed -i "s/KAMAILIO_SCSCF_IP/$KAMAILIO_SCSCF_IP/g" /etc/bind/ims.mnc001.mcc001.3gppnetwork.org
 sed -i "s/KAMAILIO_ICSCF_IP/$KAMAILIO_ICSCF_IP/g" /etc/bind/ims.mnc001.mcc001.3gppnetwork.org
